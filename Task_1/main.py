9e665fd33e73ce71539d58e077a3ec792c6df3c4
mohamedsherif301 <myosre122@gmail.com>
2025-03-04 19:07:03 +0200

    Intial Verion of Code

---
+++ Task_1/main.py
@@ -0,0 +1,10 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+prime_numbers = [num for num in range(25, 41) if is_prime(num)]
+print(prime_numbers)
