37ee39e91d14c7183dfbd454b9767d40550b6358
mohamedsherif301 <myosre122@gmail.com>
2025-03-10 15:06:58 +0200

    Removed Task_1 and Task_2 from tracking

--- Task_2/main.py
+++
@@ -1,9 +0,0 @@
-list1 = [100, 200, 300, 'A', 400, 500]
-
-all_integers = all(isinstance(item, int) for item in list1)
-
-# طباعة النتيجة
-if all_integers:
-    print("All Element is Integer")
-else:
-    print("there Element is Non Integer")
